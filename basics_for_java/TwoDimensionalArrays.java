0bbc10cbc3a8b7bda769541df2652210360eb98c
Amardeep1811 <amardeepsharma95238@gmail.com>
2026-02-10 22:29:12 +0530

    2d arrays using java and iterating thru them

--- basics_for_java/TwoDimensionalArrays.java
+++ basics_for_java/TwoDimensionalArrays.java
@@ -16,6 +16,35 @@ public class TwoDimensionalArrays {
         System.out.println("arr[0][0] = " + arr[0][0]);
         System.out.println("arr[0][1] = " + arr[0][1]);
         System.out.println("arr[0][2] = " + arr[0][2]);
+
+   
+      	// Row and Columns in Array
+        int n = 2;
+        int m = 2;
+
+      	// Array declared and initialized
+        int[][] arr1 = new int[n][m];
+
+        int it = 1;
+      
+      	// Assigning the values to array
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr1[i][j] = it;
+                it++;
+            }
+        }
+
+      	// Printing the Array
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
+                System.out.print(arr1[i][j] + " ");
+            System.out.println();
+        }
     }
 }
+
+
+
+
 
